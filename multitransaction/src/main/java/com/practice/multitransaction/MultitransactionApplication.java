010599f7c13cfb07ce1b188ccde6bfeaf511ee89
LaVi <lastvirgo266@gmail.com>
2020-07-05 23:28:27 +0900

    Init MultiTransaction

---
+++ multitransaction/src/main/java/com/practice/multitransaction/MultitransactionApplication.java
@@ -0,0 +1,13 @@
+package com.practice.multitransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitransactionApplication.class, args);
+    }
+
+}
---
+++ multitransaction/src/test/java/com/practice/multitransaction/MultitransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.multitransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
