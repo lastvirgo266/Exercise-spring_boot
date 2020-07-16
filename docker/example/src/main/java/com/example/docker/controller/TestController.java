c8d85de330e4a5b800ecb98c2c06c9b0d3042727
LaVi <lastvirgo266@naver.com>
2020-07-17 00:35:21 +0900

    Making Test

---
+++ docker/example/src/main/java/com/example/docker/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.docker.controller;
+
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+public class TestController {
+
+
+    @RequestMapping(value = "/test")
+    public ResponseEntity<?> test(){
+
+        
+
+        return new ResponseEntity
+
+    }
+
+}
